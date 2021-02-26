e7c389d887234876ddff898fa0d46c72b8df5849
Emily Schwabe <emilykschwabe@gmail.com>
2021-02-26 10:45:44 -0800

    wrote a test for cats package, passed all tests

---
+++ tests/testthat/test-cats.R
@@ -0,0 +1,8 @@
+test_that("logical operations works", {
+  expect_equal(cats(TRUE), "I love cats!")
+  expect_equal(cats(1), "I love cats!")
+  expect_equal(cats(FALSE), "I am not a cat person.")
+  expect_equal(cats(0), "I am not a cat person.")
+  
+  
+})
